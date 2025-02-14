d487bf35076756138653675e4d67f810c2184e78
gungorhafize <gungorhafizee@gmail.com>
2025-02-14 16:15:13 +0300

    add serialization for json

--- src/main/kotlin/models/ApiResponse.kt
+++ src/main/kotlin/models/ApiResponse.kt
@@ -2,9 +2,10 @@ package com.hafize.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ApiResponse( val success : Boolean,
-                        val message : String? = null,
-                        val prevPage : Int? = null,
-                        val nextPage : Int? = null,
-                        val sailorMoon : List<Character> = emptyList()
+data class ApiResponse(
+    val success : Boolean,
+    val message : String? = null,
+    val prevPage : Int? = null,
+    val nextPage : Int? = null,
+    val sailorMoon : List<Character> = emptyList()
 )
--- src/main/kotlin/models/Character.kt
+++ src/main/kotlin/models/Character.kt
@@ -1,5 +1,6 @@
 package com.hafize.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -7,9 +8,13 @@ data class Character(
     val id: Int,
     val name: String,
     val alias: String,
+    @SerialName("birth_date")
     val birthDate: String,
+    @SerialName("height_cm")
     val heightCm: Int,
+    @SerialName("power_level")
     val powerLevel: Int,
+    @SerialName("special_attacks")
     val specialAttacks: List<String>,
     val description: String,
     val image: String
